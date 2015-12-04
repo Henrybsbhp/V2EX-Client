c169f67f03950dd77e6401f76d8abab4d3e43a2c
St. Jimmy <Henrybsbhp@gmail.com>
2015-12-04 22:36:59 +0800

    Some changes.

--- V2EX/LoginViewController.m
+++ V2EX/LoginViewController.m
@@ -12,9 +12,19 @@
 #import "AFHTTPSessionManager.h"
 #import "AFHTTPRequestOperationManager.h"
 
+typedef NS_ENUM(NSInteger, V2RequestMethod) {
+    V2RequestMethodJSONGET    = 1,
+    V2RequestMethodHTTPPOST   = 2,
+    V2RequestMethodHTTPGET    = 3,
+    V2RequestMethodHTTPGETPC  = 4
+};
+
 @interface LoginViewController ()
 
 @property (nonatomic, strong) AFHTTPSessionManager *manager;
+
+@property (nonatomic, copy) NSString *userAgentMobile;
+@property (nonatomic, copy) NSString *userAgentPC;
 
 @end
 
@@ -51,7 +61,7 @@
                           NSString *once = [self findOnceInHTMLString:HTMLString];
                           NSLog(@"ONCE: %@", once);
                           
-                          [self loginWithOnce:once];
+                          [self loginWithOnce:once username:self.txtUsername.text password:self.txtPassword.text];
                           
                       } failure:^void(NSURLSessionDataTask * operation, NSError * error) {
                           NSLog(@"Error: %@", error);
@@ -104,18 +114,22 @@
     }
 }
 
-- (void)loginWithOnce:(NSString *)once
+- (void)loginWithOnce:(NSString *)once username:(NSString *)username password:(NSString *)password
 {
     NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
     for (NSHTTPCookie *cookie in [storage cookies]) {
         [storage deleteCookie:cookie];
     }
     
+    self.manager = [AFHTTPSessionManager manager];
+    self.manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    self.manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
     NSDictionary *parameters = @{
                                  @"once" : once,
                                  @"next" : @"/",
-                                 @"p" : self.txtPassword.text,
-                                 @"u" : self.txtUsername.text,
+                                 @"p" : password,
+                                 @"u" : username,
                                  };
     
     NSLog(@"PARAMETERS: %@", parameters);
@@ -128,14 +142,15 @@
     [self.manager.requestSerializer setValue:@"zh-CN,zh;q=0.8,ja;q=0.6,zh-TW;q=0.4" forHTTPHeaderField:@"Accept-Language"];
     [self.manager.requestSerializer setValue:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36" forHTTPHeaderField:@"User-Agent"];
     
-    [self.manager POST:@"https://www.v2ex.com/signin" parameters:parameters
-                                                        success:^void(NSURLSessionDataTask * task, id responseObject) {
-                                                            NSString *HTMLString = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
-                                                            NSLog(@"%@", HTMLString);
-
-                                                        }failure:^void(NSURLSessionDataTask * operation, NSError * error) {
-                                                       
-                                                        }];
+    [self requestWithMethod:V2RequestMethodHTTPPOST URLString:@"https://www.v2ex.com/signin" parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+        
+        NSString *htmlString = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"RESPONSE HTMLSTRING: %@", htmlString);
+        
+    }failure:^(NSError *error) {
+        
+    }];
     
 }
 
@@ -147,6 +162,79 @@
     } else {
         return nil;
     }
+}
+
+- (NSURLSessionDataTask *)requestWithMethod:(V2RequestMethod)method
+                                  URLString:(NSString *)URLString
+                                 parameters:(NSDictionary *)parameters
+                                    success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+                                    failure:(void (^)(NSError *error))failure  {
+    // stateBar
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    
+    // Handle Common Mission, Cache, Data Reading & etc.
+    void (^responseHandleBlock)(NSURLSessionDataTask *task, id responseObject) = ^(NSURLSessionDataTask *task, id responseObject) {
+        
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        
+        // Any common handler for Response
+        
+        //        NSLog(@"URL:\n%@", [task.currentRequest URL].absoluteString);
+        
+        
+        success(task, responseObject);
+        
+    };
+    
+    // Create HTTPSession
+    NSURLSessionDataTask *task = nil;
+    
+    [self.manager.requestSerializer setValue:self.userAgentMobile forHTTPHeaderField:@"User-Agent"];
+    
+    if (method == V2RequestMethodJSONGET) {
+        AFHTTPResponseSerializer *responseSerializer = [AFJSONResponseSerializer serializer];
+        self.manager.responseSerializer = responseSerializer;
+        task = [self.manager GET:URLString parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+            responseHandleBlock(task, responseObject);
+        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+            NSLog(@"Error: \n%@", [error description]);
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+            failure(error);
+        }];
+    }
+    if (method == V2RequestMethodHTTPGET) {
+        AFHTTPResponseSerializer *responseSerializer = [AFHTTPResponseSerializer serializer];
+        self.manager.responseSerializer = responseSerializer;
+        task = [self.manager GET:URLString parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+            responseHandleBlock(task, responseObject);
+        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+            failure(error);
+        }];
+    }
+    if (method == V2RequestMethodHTTPPOST) {
+        AFHTTPResponseSerializer *responseSerializer = [AFHTTPResponseSerializer serializer];
+        self.manager.responseSerializer = responseSerializer;
+        task = [self.manager POST:URLString parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+            responseHandleBlock(task, responseObject);
+        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+            failure(error);
+        }];
+    }
+    if (method == V2RequestMethodHTTPGETPC) {
+        AFHTTPResponseSerializer *responseSerializer = [AFHTTPResponseSerializer serializer];
+        self.manager.responseSerializer = responseSerializer;
+        [self.manager.requestSerializer setValue:self.userAgentPC forHTTPHeaderField:@"User-Agent"];
+        task = [self.manager GET:URLString parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+            responseHandleBlock(task, responseObject);
+        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+            failure(error);
+        }];
+    }
+    
+    return task;
 }
 
 - (void)alertStatus:(NSString *)msg:(NSString *)title:(int)tag
